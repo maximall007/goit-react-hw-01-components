b40a56fc671df568fe9e03644ba85c2ab28de037
max <xmaximalxx@gmail.com>
2020-02-02 13:19:24 +0200

    add buid

---
+++ build/precache-manifest.f8a4dfde5499c302323cb00c0e648a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7953050630aa4434dfb1f07175707bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0096c23e15d77ea8d6dc",
+    "url": "/static/css/main.34d63de7.chunk.css"
+  },
+  {
+    "revision": "c9b4603ab169b6a847c3",
+    "url": "/static/js/2.7ed1cf1c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7ed1cf1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0096c23e15d77ea8d6dc",
+    "url": "/static/js/main.e25dd395.chunk.js"
+  },
+  {
+    "revision": "3f465c566c8a11e30a11",
+    "url": "/static/js/runtime-main.8ca8bd23.js"
+  }
+]);
