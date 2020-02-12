072e56e4c2b101dfd8858f41c2cd8c19fa89e938
max <xmaximalxx@gmail.com>
2020-02-12 20:29:33 +0200

    Updates

---
+++ precache-manifest.5ca2cb93761af53ac28b880870dfc5b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a21d6908447ecb4294e48c95a2bd456",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "7e49cec0a36ea1035d88",
+    "url": "/goit-react-hw-01-components/static/css/main.2d89a10b.chunk.css"
+  },
+  {
+    "revision": "c9b4603ab169b6a847c3",
+    "url": "/goit-react-hw-01-components/static/js/2.7ed1cf1c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/static/js/2.7ed1cf1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e49cec0a36ea1035d88",
+    "url": "/goit-react-hw-01-components/static/js/main.59fef4e5.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
